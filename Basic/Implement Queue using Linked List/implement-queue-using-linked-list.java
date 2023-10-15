bd0f76b4ee06000ebd7aef3c9254bbfed2652547
Adarsh _007 <70956257+Adarsh221999@users.noreply.github.com>
2023-10-15 18:38:43 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Implement Queue using Linked List/implement-queue-using-linked-list.java
@@ -0,0 +1,105 @@
+//{ Driver Code Starts
+import java.util.*;
+class QueueNode
+{
+	int data;
+	QueueNode next;
+	QueueNode(int a)
+	{
+	    data = a;
+	    next = null;
+	}
+}
+
+class GfG {
+	public static void main(String args[])
+	{
+		 Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+		 {
+			MyQueue obj = new MyQueue();
+			int Q = sc.nextInt();
+			while(Q>0)
+			{
+				int QueryType = 0;
+				QueryType = sc.nextInt();
+				if(QueryType == 1)
+				{
+					int a = sc.nextInt();
+					
+					obj.push(a);
+					
+				}else if(QueryType == 2)
+				{
+				System.out.print(obj.pop()+" ");
+				}
+				Q--;
+			}
+			System.out.println("");
+			t--;
+		 }
+	}
+}
+
+
+
+
+
+// } Driver Code Ends
+
+
+/*The structure of the node of the queue is
+class QueueNode
+{
+	int data;
+	QueueNode next;
+	QueueNode(int a)
+	{
+	    data = a;
+	    next = null;
+	}
+}*/
+
+class MyQueue
+{
+    QueueNode front, rear;
+    
+    //Function to push an element into the queue.
+	void push(int a)
+	{
+	     QueueNode newnode=new QueueNode(a);
+        
+        if(front==null){
+            front=rear=newnode;
+        }
+        rear.next=newnode;
+        rear=rear.next;
+        
+	}
+	
+    //Function to pop front element from the queue.
+	int pop()
+	{ 
+	        //Queue is empty
+        if(front==null){
+            return -1;
+        }
+        
+        int top=front.data;
+        
+        //only one element
+        if(rear==front){
+            rear=front=null;
+        }
+        
+        else{
+            front=front.next;
+        }
+        return top;
+	}
+}
+
+
+
+
